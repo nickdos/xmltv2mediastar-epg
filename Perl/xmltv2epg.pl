6585acf618c85f10bc88747b730199b743c1cf83
nickdos <nickdos@8199d2d9-151a-0410-b199-451139a771d9>
2006-10-05 23:56:12 +0000

    Added GPL preamble and LICENSE.txt file to package.

--- Perl/xmltv2epg.pl
+++ Perl/xmltv2epg.pl
@@ -4,7 +4,21 @@
 #
 # copyright (c) 2006 Nick dos Remedios - nick at remedios-cole.id.au
 # 
-# version 0.$Revision$, NdR $Date$
+# xmltv2mediastar is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# xmltv2mediastar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Cambia Sequence; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# Version 0.$Revision$, NdR $Date$
 #
 use strict;
 use XML::Simple;
