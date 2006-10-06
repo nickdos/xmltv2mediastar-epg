fa6ab694d1c63685a4e9fb44cbdeed8d36aad010
nickdos <nickdos@8199d2d9-151a-0410-b199-451139a771d9>
2006-10-06 00:01:41 +0000

    Minor typo

--- trunk/Perl/xmltv2epg.pl
+++ trunk/Perl/xmltv2epg.pl
@@ -18,7 +18,7 @@
 # along with Cambia Sequence; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
-# Version 0.$Revision$, NdR $Date$
+# Version $Revision$, NdR $Date$
 #
 use strict;
 use XML::Simple;
